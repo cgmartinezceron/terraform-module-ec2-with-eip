173790c440a96fd4f08f5ba797678994b3644a8d
Your Name <you@example.com>
2019-01-27 05:48:47 -0800

    first commit

---
+++ sg.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "sg" {
+  name        = "${var.project_name}-${var.environment}"
+  description = "Segurity group for ${var.project_name}-${var.environment}"
+  vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
