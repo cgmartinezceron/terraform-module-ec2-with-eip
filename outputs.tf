72b9c8263caef2859c69f63ed06351eae56150c0
cgmartinezceron <71668683+cgmartinezceron@users.noreply.github.com>
2020-10-10 17:11:56 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "sg_id"{
-  value = "${aws_security_group.sg.id}"
+  value = aws_security_group.sg.id
 }
 
 
 output "eip"{
-  value = "${aws_eip.eip.public_ip}"
+  value = aws_eip.eip.public_ip
 }
