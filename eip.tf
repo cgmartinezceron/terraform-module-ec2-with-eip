a3c408ed6229b34d27a08ee0fe96e49f7d480ddc
cgmartinezceron <71668683+cgmartinezceron@users.noreply.github.com>
2020-10-10 17:11:03 -0500

    Update eip.tf

--- eip.tf
+++ eip.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "eip" {
   vpc      = true
   
-  instance = "${aws_instance.web.id}"
+  instance = aws_instance.web.id
   
   tags {
     Name = "${var.project_name}-${var.environment}"
